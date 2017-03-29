6519d17d347b36cc5b38293fa990e72ac73aa0f0
Leonard <aktasm2005@hotmail.com>
2017-03-29 16:08:00 +0300

    33 - Angular nested scopes and controller as syntax - 02

--- kudvendat/ex33.js
+++ kudvendat/ex33.js
@@ -3,12 +3,12 @@
  */
 var app = angular
     .module("Demo", [])
-    .controller("countryController", function ($scope) {
-        $scope.name = "India";
+    .controller("countryController", function () {
+        this.name = "India";
     })
-    .controller("stateController", function ($scope) {
-        $scope.name = "Maharashtra";
+    .controller("stateController", function () {
+        this.name = "Maharashtra";
     })
-    .controller("cityController", function ($scope) {
-        $scope.name = "Mumbai";
+    .controller("cityController", function () {
+        this.name = "Mumbai";
     });
